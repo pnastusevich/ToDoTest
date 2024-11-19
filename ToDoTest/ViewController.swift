6493e3e78ee5d471f7c3f3643a67ec897d568b7f
pnastusevich <pasha.nastusevich@gmail.com>
2024-11-19 18:49:40 +0300

    Initial Commit

---
+++ ToDoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoTest
+//
+//  Created by Паша Настусевич on 19.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
